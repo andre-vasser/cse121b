55a2e30e1c0c165ceef6af43a0c2abce26ef91d7
andre-vasser <vas17001@byui.edu>
2024-03-27 18:28:08 -0600

    Task 2

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -4,19 +4,23 @@
 
 /* Step 2 - Variables */
 let fullName = 'Andre Vasser';
+let currentyear = '2024';
+let profilePicture = "../images/Andre.png";
 
 
 
 /* Step 3 - Element Variables */
-
-
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+let yearElement = document.querySelector("#year");
+const imageElement = document.getElementById("profilePicture");
 
 
 
 /* Step 4 - Adding Content */
-
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = `${currentyear}`;
+imageElement.setAttribute('src', profilePicture);
 
 
 
